ef1d7d761a9838003841976931efc604b33f3d22
maemura suzu <maemura.suz.14@gmail.com>
2026-01-20 09:46:15 +0900

    オブジェクト指向入門

---
+++ 2.test-object-oriented/src/Exam2.java
@@ -0,0 +1,9 @@
+public class Exam2 {
+    public static void main(String[] args) {
+        String[] dates = {"月", "火", "水", "木", "金", "土", "日"};
+
+        for(String date:dates){
+            System.out.println(date);
+        }
+    }
+}
---
+++ 2.test-object-oriented/src/Student.java
@@ -0,0 +1,40 @@
+public class Student {
+    int energy;
+    void sleep(){
+        energy = energy + 50;
+        if(energy >= 100){
+            energy = 100;
+        }
+        else{
+            //処理なし
+        }
+        System.out.println("寝ました。エネルギーが" + energy + "になりました。");
+        if(energy == 100){
+            System.out.println("フルパワー！");
+        }
+        else{
+            //処理なし
+        }
+    }
+
+    void study(){
+        energy = energy - 30;
+        if(energy <= 0){
+            energy = 0;
+        }
+        else{
+            //処理なし
+        }
+        System.out.println("勉強しました。エネルギーが" + energy + "になりました。");
+        if(energy == 0){
+            System.out.println("寝てください。");
+        }
+        else{
+            //処理なし
+        }
+    }
+
+    Student(int newEnergy){
+        energy = newEnergy;
+    }
+}
