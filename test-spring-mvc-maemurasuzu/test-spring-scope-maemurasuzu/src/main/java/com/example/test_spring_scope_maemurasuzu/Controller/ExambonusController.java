4db5b1d37222e3a47f2049f5c1e622b726f58299
maemura suzu <maemura.suz.14@gmail.com>
2026-02-12 11:42:57 +0900

    SQL

--- test-spring-mvc-maemurasuzu/test-spring-scope-maemurasuzu/src/main/java/com/example/test_spring_scope_maemurasuzu/Controller/ExambonusController.java
+++ test-spring-mvc-maemurasuzu/test-spring-scope-maemurasuzu/src/main/java/com/example/test_spring_scope_maemurasuzu/Controller/ExambonusController.java
@@ -1,39 +1,39 @@
-package com.example.test_spring_scope_maemurasuzu.Controller;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+// package com.example.test_spring_scope_maemurasuzu.Controller;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.ModelAttribute;
+// import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.test_spring_scope_maemurasuzu.Form.UserForm;
-import com.example.test_spring_scope_maemurasuzu.domain.User;
+// import com.example.test_spring_scope_maemurasuzu.Form.UserForm;
+// import com.example.test_spring_scope_maemurasuzu.domain.User;
 
-@Controller
-@RequestMapping("/exam")
-public class ExambonusController {
-    @ModelAttribute
-    public UserForm setupForm(){
-        return new UserForm();
-    }
+// @Controller
+// @RequestMapping("/exam")
+// public class ExambonusController {
+//     @ModelAttribute
+//     public UserForm setupForm(){
+//         return new UserForm();
+//     }
 
-    @RequestMapping("/bonus-input")
-    public String index(){
-        return "exam-bonus-input";
-    }
+//     @RequestMapping("/bonus-input")
+//     public String index(){
+//         return "exam-bonus-input";
+//     }
 
-    @RequestMapping("/bonus-input")
-    public String result(UserForm form, String password, Model model){
-        User user = new User();
-        user.setName(form.getName());
-        user.setEmail(form.getEmail());
-        user.setAge(form.getAge());
-        if("yamada@sample.com".equals(form.getEmail()) && "yamayama".equals(password)){
-            model.addAttribute("message", "成功");
-            model.addAttribute("user", user);
-        }
-        else{
-            model.addAttribute("message", "失敗");
-        }
-        return "exam-bonus-result";
+//     @RequestMapping("/bonus-input")
+//     public String result(UserForm form, String password, Model model){
+//         User user = new User();
+//         user.setName(form.getName());
+//         user.setEmail(form.getEmail());
+//         user.setAge(form.getAge());
+//         if("yamada@sample.com".equals(form.getEmail()) && "yamayama".equals(password)){
+//             model.addAttribute("message", "成功");
+//             model.addAttribute("user", user);
+//         }
+//         else{
+//             model.addAttribute("message", "失敗");
+//         }
+//         return "exam-bonus-result";
 
-    }
-}
+//     }
+// }
